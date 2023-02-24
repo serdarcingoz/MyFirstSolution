36c0733e24c9311fb39e3bab66e77b66dee1d920
zugzw <zugzw@LAPTOP-SBPSOHG1>
2023-02-24 23:04:26 +0300

    Proje dosyası ekle.

---
+++ HelloWorldTutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lütfen Serdar, İbo değerlerinden birini girin");
+
+
+
+var input = Console.ReadLine();
+
+
+if (input == "Serdar")
+{
+    Console.WriteLine("Cingöz");
+}
+else if (input == "İbo")
+{
+    Console.WriteLine("Şenol");
+
+}
+else
+{
+    Console.WriteLine("asçdjkbadlkbad");
+
+}
+Console.WriteLine("Hello, World!");
